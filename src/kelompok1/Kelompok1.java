af084cb597d417bb9ef1f3b8713c963bebda1f9b
dwandika <mochamaddwiandika2@gmail.com>
2024-09-17 14:57:29 +0700

    first

---
+++ src/kelompok1/Kelompok1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kelompok1;
+
+/**
+ *
+ * @author USER
+ */
+public class Kelompok1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
